67819996db8d7a5e3e70aa847d8d2fc609d203d6
Pradeep Val <pradeephann09@gmail.com>
2022-05-17 00:02:55 +0530

    changes  made

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link
+  rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"
+/>
     <style>
        *{
          font-family: 'Poppins', sans-serif;
@@ -15,6 +18,16 @@
         .h-font{
          font-family: 'Merienda', cursive;
         }
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type=number] {
+  -moz-appearance: textfield;
+}
      </style>
 </head>
 <body>
@@ -44,10 +57,10 @@
                 </li>
         </ul>
         <div class="d-flex">
-                <button type="button" class="btn btn-outline-dark shadow-none me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#loginModal">
+                <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
                    Login
                 </button>
-                 <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
+                 <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#RegisterModal">
                    Register
                 </button>
         </div>
@@ -87,34 +100,96 @@
  </div>
 
   <div class="modal fade" id="RegisterModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-   <div class="modal-dialog">
+   <div class="modal-dialog modal-lg">
      <div class="modal-content">
        <form >
          <div class="modal-header">
            <h5 class="modal-title d-flex align-items-center">
-             <i class="bi bi-person-circle fs-3 me-2"></i> User Login
+             <i class="bi bi-person-lines-fill fs-3 me-2"></i> User Registration
            </h5>
            <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
          </div>
          <div class="modal-body">
-           <div class="mb-3">
-                <label  class="form-label">Email address</label>
+           <span class="badge badge-light text-dark mb-3 text-wrap lh-base ">
+             Note: your details must match with your ID ( Aadhaar card, passport, driving license, etc.)
+             that will be required during check-in.
+            </span>
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-6 ps-0">
+                        <label  class="form-label">Name</label>
+                <input type="text" class="form-control shadow-none">
+                </div>
+                 <div class="col-md-6 mb-3">
+                        <label  class="form-label">Email</label>
                 <input type="email" class="form-control shadow-none">
-           </div>
-           <div class="mb-4">
-                <label  class="form-label">Password</label>
-                <input type="password" class="form-control shadow-none mb-2">
-           </div>
-           <div class="d-flex align-items-center justify-content-between mb-2">
-                <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
-                <a href="javascript: void(0)" class="text-secondary text-decoration-none" >Forgot Password?</a>
-           </div>
-         </div>
+                </div>
+                 <div class="col-md-6 ps-0 mb-3">
+                        <label  class="form-label">phone number</label>
+                <input type="number" class="form-control shadow-none">
+                </div>
+                 <div class="col-md-6 p-0 mb-3">
+                        <label  class="form-label">picture</label>
+                <input type="file" class="form-control shadow-none">
+                </div>
+                <div class="col-md-12 p-0 mb-3">
+                        <label  class="form-label">Address</label>
+                 <textarea class="form-control shadow none" rows="1"></textarea>
+                </div>
+                  <div class="col-md-6 ps-0 mb-3">
+                        <label  class="form-label">pincode</label>
+                <input type="number" class="form-control shadow-none">
+                </div>
+                 <div class="col-md-6 p-0 mb-3">
+                        <label  class="form-label">Date of birth</label>
+                <input type="date" class="form-control shadow-none">
+                </div>
+                  <div class="col-md-6 ps-0 mb-3">
+                        <label  class="form-label">Password</label>
+                <input type="password" class="form-control shadow-none">
+                </div>
+                 <div class="col-md-6 p-0 mb-3">
+                        <label  class="form-label">Confirm Password</label>
+                <input type="password" class="form-control shadow-none">
+                </div>
+              </div>
+            </div>
+<div class="text-center My-1">
+   <button type="submit" class="btn btn-dark shadow-none">REGISTER</button>
+</div>
+</div>
         </form>
      </div>
    </div>
  </div>
 
+<div class="container-fluid">
+  <div class="swiper swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </div>
+        <div class="swiper-slide">
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </div>
+        <div class="swiper-slide">
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </div>
+        <div class="swiper-slide">
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </div>
+      </div>
+    </div>
+</div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+    <script>
+      var swiper = new Swiper(".swiper-container", {
+        spaceBetween: 30,
+        effect: "fade",
+      });
+    </script>
 </body>
 </html>
