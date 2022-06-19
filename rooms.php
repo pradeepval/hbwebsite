0d85031b724b1c2d512df24551a86840b050a874
Pradeep Val <pradeephann09@gmail.com>
2022-06-19 10:44:53 +0530

    Changes done

--- rooms.php
+++ rooms.php
@@ -21,28 +21,129 @@
 <div class="container">
   <div class="row">
 
-    <div class="col-lg-3">
+    <div class="col-lg-3 col-md-12 mb-lg-0 mb-4 px-lg--0">
       <nav class="navbar navbar-expand-lg bg-light  bg-white rounded shadow">
           <div class="container-fluid flex-lg-column align-items-stretch">
          <h4 class="mt-2">FILTERS</h4>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#filterdropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#filterdropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon text-dark"></span>
             </button>
-            <div class="collapse navbar-collapse text-dark" id="filterdropdown">
+            <div class="collapse navbar-collapse text-dark flex-column align-items-stretch" id="filterdropdown">
               <div class="border bg-light p-3 rounded mb-3">
                 <h5 class="mb-3" style="font-size: 18px">CHECK AVAILABILITY</h5>
                  <label class="form-label" style="font-weight: 500"
                   >Check-in</label
                 >
                 <input type="date" class="form-control shadow-none" />
+                  <label class="form-label" style="font-weight: 500"
+                  >Check-out</label
+                >
+                <input type="date" class="form-control shadow-none" />
+              </div>
+              <div class="border bg-light p-3 rounded mb-3">
+                <h5 class="mb-3" style="font-size: 18px">FACILITIES</h5>
+                <div class="mb-2">
+                  <input type="checkbox" id="f1" class="form-check-input shadow-none me-1" />
+                  <label class="form-check-label" style="font-weight: 500"
+                  >facility one</label
+                >
+                </div>
+                 <div class="mb-2">
+                  <input type="checkbox" id="f2" class="form-check-input shadow-none me-1" />
+                  <label class="form-check-label" style="font-weight: 500"
+                  >facility two</label
+                >
+                </div>
+                 <div class="mb-2">
+                  <input type="checkbox" id="f3" class="form-check-input shadow-none me-1" />
+                  <label class="form-check-label" style="font-weight: 500"
+                  >facility three</label
+                >
+                </div>
+
+              </div>
+             <div class="border bg-light p-3 rounded mb-3">
+                <h5 class="mb-3" style="font-size: 18px">GUESTS</h5>
+            <div class="d-flex">
+              <div class="me-2">
+               <label class="form-label" style="font-weight: 500"
+                  >Adults</label>
+                   <input type="number" class="form-control shadow-none" />
+              </div>
+              <div>
+               <label class="form-label" style="font-weight: 500"
+                  >Children</label>
+                   <input type="number" class="form-control shadow-none" />
+              </div>
+                </div>
               </div>
             </div>
           </div>
         </nav>
     </div>
 
-  </div>
-</div>
+   <div class="col-lg-9 col-md-12 px-4">
+      <div class="card mb-4 border-0 shadow">
+        <div class="row g-0 p-3 align-items-center">
+          <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
+            <img src="images/rooms/1.jpg" class="img-fluid rounded">
+          </div>
+        <div class="col-md-5 px-lg-3 px-md-3 px-0">
+          <h5 class=" mb-3">Simple Room Name
+          </h5>
+              <div class="features mb-3">
+                      <h6 class="mb-1">Features</h6>
+                      <span class="badge badge-light text-dark text-wrap">
+                        2 Rooms
+                      </span>
+                      <span class="badge badge-light text-dark text-wrap">
+                        1 Bathroom
+                      </span>
+                      <span class="badge badge-light text-dark text-wrap">
+                        1 Balcony
+                      </span>
+                      <span class="badge badge-light text-dark text-wrap">
+                        3 sofas
+                      </span>
+              </div>
+              <div class="facilities mb-3">
+                      <h6 class="mb-1">Facilities</h6>
+                      <span class="badge badge-light text-dark text-wrap">
+                        Wifi
+                      </span>
+                      <span class="badge badge-light text-dark text-wrap">
+                        Television
+                      </span>
+                      <span class="badge badge-light text-dark text-wrap"> AC </span>
+                      <span class="badge badge-light text-dark mb-2 text-wrap">
+                        Room heater
+                      </span>
+              </div>
+                  <div class="guests">
+                      <h6 class="mb-1">Guests</h6>
+                      <span class="badge badge-light text-dark text-wrap">
+                      5 Adults
+                      </span>
+                      <span class="badge badge-light text-dark text-wrap">
+                      4 Children
+                      </span>
+                  </div>
+         </div>
+        <div class="col-md-2 text-center">
+            <h6 class="mb-4">₹200 per night</h6>
+              <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2"
+                        >Book Now</a
+                      >
+                      <a href="#" class="btn btn-sm w-100 btn-outline-dark shadow-none"
+                        >More details</a
+                      >
+          </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
 
 
   <?php require 'inc/footer.php';?>
