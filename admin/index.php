7d26080ae033f94629f8b237f1da83a77d1e1323
Pradeep Val <pradeephann09@gmail.com>
2022-06-20 20:29:33 +0530

    admin panel cahnges

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@ require 'inc/db_config.php';
 
 session_start();
 if ((isset($_SESSION['adminLogin']) && $_SESSION['adminLogin'] == true)) {
-  redirect('dashboard.php');
+ redirect('dashboard.php');
 }
 ?>
 
@@ -47,20 +47,20 @@ if ((isset($_SESSION['adminLogin']) && $_SESSION['adminLogin'] == true)) {
 <?php
 
 if (isset($_POST['login'])) {
-  $frm_data = filteration($_POST);
+ $frm_data = filteration($_POST);
 
-  $query  = "SELECT * FROM `admin_cred` WHERE `admin_name`=? AND `admin_pass`=?";
-  $values = [$frm_data['admin_name'], $frm_data['admin_pass']];
+ $query  = "SELECT * FROM `admin_cred` WHERE `admin_name`=? AND `admin_pass`=?";
+ $values = [$frm_data['admin_name'], $frm_data['admin_pass']];
 
-  $res = select($query, $values, "ss");
-  if ($res->num_rows == 1) {
-    $row                    = mysqli_fetch_assoc($res);
-    $_SESSION['adminLogin'] = true;
-    $_SESSION['adminId']    = $row['sr_no'];
-    redirect('dashboard.php');
-  } else {
-    alert('error', 'Login failed - Invalid Credentials!');
-  }
+ $res = select($query, $values, "ss");
+ if ($res->num_rows == 1) {
+  $row                    = mysqli_fetch_assoc($res);
+  $_SESSION['adminLogin'] = true;
+  $_SESSION['adminId']    = $row['sr_no'];
+  redirect('dashboard.php');
+ } else {
+  alert('error', 'Login failed - Invalid Credentials!');
+ }
 
 }
 
