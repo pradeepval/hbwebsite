df076317290e8b347b245c657d6ea43b1d5d9a9e
samsuits <satish.samala@gmail.com>
2022-06-28 20:43:09 +1200

    Merge branch 'master' of github.com:pradeepval/hbwebsite

--- admin/settings.php
+++ admin/settings.php
@@ -7,75 +7,74 @@ adminLogin();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel - settings</title>
-    <?php require 'inc/links.php';?>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Panel - settings</title>
+  <?php require 'inc/links.php'; ?>
 </head>
+
 <body class="bg-light ">
 
-<?php require 'inc/header.php';?>
+  <?php require 'inc/header.php'; ?>
 
 
-<div class="container-fluid" id="main-content">
+  <div class="container-fluid" id="main-content">
     <div class="row">
-        <div class="col-lg-10 ms-auto p-4 over-flow ">
-          <h3 CLASS="mb-4">SETTINGS</h3>
+      <div class="col-lg-10 ms-auto p-4 over-flow ">
+        <h3 CLASS="mb-4">SETTINGS</h3>
 
         <!--  General settings section -->
-    <div class="card border-0 shadow-sm mb-4 ">
-        <div class="card-body">
+        <div class="card border-0 shadow-sm mb-4 ">
+          <div class="card-body">
             <div class="d-flex align-items-center justify-content-between mb-3">
               <h5 class="card-title m-0">General Settings</h5>
-                <button type="button" class="btn btn-dark shadow-none btn-sn" data-bs-toggle="modal" data-bs-target="#general-s">
+              <button type="button" class="btn btn-dark shadow-none btn-sn" data-bs-toggle="modal" data-bs-target="#general-s">
                 <i class="bi bi-pencil-square"></i>Edit
-                </button>
+              </button>
             </div>
             <h6 class="card-subtitle mb-1 fw-bold">Side title</h6>
-            <p class="card-text" id ="site_title"></p>
+            <p class="card-text" id="site_title"></p>
             <h6 class="card-subtitle mb-1 fw-bold">About us</h6>
-             <p class="card-text" id ="site_about"></p>
+            <p class="card-text" id="site_about"></p>
+          </div>
         </div>
-    </div>
 
-       <!--  General settings modal section -->
+        <!--  General settings modal section -->
 
-    <div class="modal fade" id="general-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal fade" id="general-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div class="modal-dialog">
             <form>
-             <div class="modal-content">
+              <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">General Settings</h5>
+                  <h5 class="modal-title">General Settings</h5>
 
                 </div>
                 <div class="modal-body">
                   <div class="mb-3">
                     <label class="form-label">Site Title</label>
-                    <input type="text" name="site_title" id ="site_title_inp" class="form-control shadow-none" />
+                    <input type="text" name="site_title" id="site_title_inp" class="form-control shadow-none" />
                   </div>
-                   <div class="mb-3">
+                  <div class="mb-3">
                     <label class="form-label">About us</label>
-                    <textarea name="site_about"  id ="site_about_inp"
-                      class="form-control shadow none"
-                      rows="6"
-                    ></textarea>
+                    <textarea name="site_about" id="site_about_inp" class="form-control shadow none" rows="6"></textarea>
                   </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" onclick ="site_title.value = general_data.site_title ,  site_about.value = general_data.site_about" class="btn btn-secondary" data-bs-dismiss="modal">CANCEL</button>
-                    <button type="button" onclick ="upd_general(site_title.value, site_about.value)" class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                  <button type="button" onclick="site_title.value = general_data.site_title ,  site_about.value = general_data.site_about" class="btn btn-secondary" data-bs-dismiss="modal">CANCEL</button>
+                  <button type="button" onclick="upd_general(site_title.value, site_about.value)" class="btn custom-bg text-white shadow-none">SUBMIT</button>
                 </div>
-                </div>
+              </div>
             </form>
-         </div>
-    </div>
+          </div>
+        </div>
 
 
-       <!--  shutdown section -->
-       <div class="card border-0 shadow-sm">
-        <div class="card-body">
+        <!--  shutdown section -->
+        <div class="card border-0 shadow-sm">
+          <div class="card-body">
             <div class="d-flex align-items-center justify-content-between mb-3">
               <h5 class="card-title m-0">Shutdown Website</h5>
               <div class="form-check form-switch">
@@ -84,111 +83,105 @@ adminLogin();
                 </form>
               </div>
             </div>
-             <p class="card-text" >
-             No customers will be allowed to book hotel room, when shutdown mode is  turned on</p>
+            <p class="card-text">
+              No customers will be allowed to book hotel room, when shutdown mode is turned on</p>
+          </div>
         </div>
+
+      </div>
     </div>
-
-        </div>
-    </div>
-</div>
+  </div>
 
 
 
 
-  <?php require 'inc/script.php';?>
+  <?php require 'inc/script.php'; ?>
   <script>
-  let general_data;
+    let general_data;
 
-  function get_general()
-  {
-    let site_title = document.getElementById('site_title');
-    let site_about = document.getElementById('site_about');
+    function get_general() {
+      let site_title = document.getElementById('site_title');
+      let site_about = document.getElementById('site_about');
 
-    let site_title_inp = document.getElementById('site_title_inp');
-    let site_about_inp = document.getElementById('site_about_inp');
+      let site_title_inp = document.getElementById('site_title_inp');
+      let site_about_inp = document.getElementById('site_about_inp');
 
-    let shutdown_toggle = document.getElementById('shutdown-toggle');
+      let shutdown_toggle = document.getElementById('shutdown-toggle');
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST","ajax/settings_crud.php",true);
-    xhr.setRequestHeader('content-type','application/x-www-form-urlencoded');
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "ajax/settings_crud.php", true);
+      xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
 
-    xhr.onload = function() {
-      general_data = JSON.parse(this.responseText);
+      xhr.onload = function() {
+        general_data = JSON.parse(this.responseText);
 
-      site_title.innerText = general_data.site_title;
-       site_about.innerText = general_data.site_about;
+        site_title.innerText = general_data.site_title;
+        site_about.innerText = general_data.site_about;
 
-       site_title_inp.value = general_data.site_title;
-       site_about_inp.value = general_data.site_about;
+        site_title_inp.value = general_data.site_title;
+        site_about_inp.value = general_data.site_about;
 
-        if(general_data.shutdown == 0){
+        if (general_data.shutdown == 0) {
           shutdown_toggle.checked = false;
           shutdown_toggle.value = 0;
-        }
-        else{
-           shutdown_toggle.checked = true;
-           shutdown_toggle.value = 1;
+        } else {
+          shutdown_toggle.checked = true;
+          shutdown_toggle.value = 1;
         }
       }
-      xhr . send('get_general');
+      xhr.send('get_general');
 
     }
 
 
 
 
-  function upd_general(site_title_val, site_about_val)
-{
-  let xhr = new XMLHttpRequest();
-    xhr.open("POST","ajax/settings_crud.php",true);
-    xhr.setRequestHeader('content-type','application/x-www-form-urlencoded');
+    function upd_general(site_title_val, site_about_val) {
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "ajax/settings_crud.php", true);
+      xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
 
-    xhr.onload = function() {
+      xhr.onload = function() {
 
-      var myModal= document.getElementById('general-s')
-       var modal = bootstrap.Modal.getInstance(myModal)
+        var myModal = document.getElementById('general-s')
+        var modal = bootstrap.Modal.getInstance(myModal)
         modal.hide();
 
-       if(this.responseText ==1)
-       {
-        alert('success', 'changes saved!');
-         get_general();
-       }
-       else {
-        alert('error', ' no changes made!');
+        if (this.responseText == 1) {
+          alert('success', 'changes saved!');
+          get_general();
+        } else {
+          alert('error', ' no changes made!');
 
-       }
+        }
+      }
+
+      xhr.send('site_title=' + site_title_val + '&site_about=' + site_about_val + '&upd_general=1');
     }
 
-    xhr.send('site_title='+site_title_val+'&site_about='+site_about_val+'&upd_general=1');
-}
+    function upd_shutdown(val) {
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "ajax/settings_crud.php", true);
+      xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
 
-  function upd_shutdown(val){
-        let xhr = new XMLHttpRequest();
-    xhr.open("POST","ajax/settings_crud.php",true);
-    xhr.setRequestHeader('content-type','application/x-www-form-urlencoded');
+      xhr.onload = function() {
+        if (this.responseText == 1) {
+          alert('success', 'site has been shutdown!');
 
-    xhr.onload = function() {
-       if(this.responseText == 1)
-       {
-        alert('success', 'site has been shutdown!');
+        } else {
+          alert('success', 'shutdown mode off!');
 
-       }
-       else {
-        alert('success', 'shutdown mode off!');
+        }
+        get_general();
+      }
 
-       }
-       get_general();
+      xhr.send('upd_shutdown=' + val);
     }
 
-    xhr.send('upd_shutdown=' +val);
+    window.onload = function() {
+      get_general();
     }
-
-  window.onload = function(){
-  get_general();
-  }
-</script>
+  </script>
 </body>
+
 </html>
